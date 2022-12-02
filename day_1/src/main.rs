9e83286170b99bdf9bf7e63a9ddc3be312110da2
Brian Jubelirer <brian2386@gmail.com>
2022-12-01 19:55:05 -0500

    day 1

---
+++ day_1/src/main.rs
@@ -0,0 +1,38 @@
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+    let mut iter = contents.lines();
+
+    let mut top_3 = vec![0,0,0];
+    let mut curr = 0;
+    while let Some(line) = iter.next() {
+        if let Ok(num) = line.parse::<i32>() {
+            curr += num;
+            let lowest = min(&top_3);
+            if curr > lowest{
+                let pos = top_3.iter().position(|&x| x == lowest);
+                match pos {
+                    Some(x) => top_3[x] = curr,
+                    None => println!("{:?} {:?}", top_3, curr),
+                }
+            }
+        } else {
+            curr = 0;
+        }
+    }
+    print!("{:?}", top_3);
+    let sum = top_3.iter().sum::<i32>();
+    print!("{:?}", sum);
+}
+
+
+fn min(v: &Vec<i32>)  -> i32 {
+    let mut min = std::i32::MAX;
+    for i in 0..v.len() {
+        if v[i] < min {
+            min = v[i];
+        }
+    }
+    min
+}
